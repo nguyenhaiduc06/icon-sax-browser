edd31cb46d7e6d255e8a73bc0a29ac97fba41dbc
Hai Duc Nguyen <nguyenhaiduc06@gmail.com>
2021-10-03 00:11:24 +0700

    update: Icon

--- src/components/Icon/index.js
+++ src/components/Icon/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 export default function Icon({ iconType, iconName }) {
   return (
-    <div class="flex flex-col items-center justify-center bg-gray-200 rounded p-2 cursor-pointer">
+    <div class="flex flex-col items-center justify-center rounded py-4 px-2 cursor-pointer bg-white border border-transparent hover:border-blue-400 ease-in-out transition-all duration-300">
       <img src={require(`../../assets/${iconType}/${iconName}`).default} alt={iconName} />
-      <p class="text-center text-xs mt-2 truncate w-full">{iconName.split('.')[0]}</p>
+      <p class="text-center text-xs mt-2 truncate w-full text-gray-400">{iconName.split('.')[0]}</p>
     </div>
   );
 }
