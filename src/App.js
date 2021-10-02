a7aa0ade0df68d1b50eb0959fdca366224ddf47c
Hai Duc Nguyen <nguyenhaiduc06@gmail.com>
2021-10-03 00:18:21 +0700

    update: select icon type

--- src/App.js
+++ src/App.js
@@ -13,15 +13,18 @@ function App() {
     <div class="bg-gray-100 min-h-screen">
       <NavBar search={search} setSearch={setSearch} />
       <div class="m-auto max-w-screen-lg bg-white rounded-lg mt-4 p-4">
-        <div class="flex flex-row items-center justify-between">
-          <div>
-            Icons
-          </div>
-          <div class="flex flex-row items-center justify-between">
-            {Object.keys(IconTypes).map((key, index) => (
-              <div class={`mx-2 cursor-pointer hover:text-blue-400 ${key==iconType ? 'text-blue-500' : ''}`} key={index} onClick={() => setIconType(key)}>{capitalize(key)}</div>
-            ))}
-          </div>
+        <div class="flex flex-row items-center">
+          {Object.keys(IconTypes).map((key, index) => (
+            <div
+              class={`mr-2 cursor-pointer hover:text-blue-400 ${
+                key == iconType ? 'text-blue-500' : ''
+              }`}
+              key={index}
+              onClick={() => setIconType(key)}
+            >
+              {capitalize(key)}
+            </div>
+          ))}
         </div>
         <div class="grid grid-cols-8 gap-4 mt-4">
           {IconNames[iconType].map((iconName, index) => (
